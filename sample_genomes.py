60a0b97e04e4018aff8d50b74bd4abdefe114dbf
Luiz Thiberio Rangel <thiberio@lua>
2017-11-01 20:10:20 -0200

    added filter to genomes without ".faa" file (ftplib) and fixed duplicated sampling

--- sample_genomes.py
+++ sample_genomes.py
@@ -28,22 +28,39 @@ import numpy as np
 import multiprocessing
 import random
 import itertools
+import ftplib as ftp
+
+
+def test_ftp_path( path ):
+    path = path.replace('ftp://', '')
+    ncbi = ftp.FTP('ftp.ncbi.nlm.nih.gov')
+    ncbi.login()
+    try:
+        ncbi.cwd( path.replace('ftp.ncbi.nlm.nih.gov/', '') )
+    except:
+        return False
+    assembly_files = ncbi.nlst()
+    for assembly_file in assembly_files:
+        if assembly_file.endswith('protein.faa.gz'):
+            return True
+    ncbi.quit()
+    return False
 
 def select_taxa_with_best_data( tmp_df, taxon_rank ):
-    df = tmp_df.copy()
+    df             = tmp_df.copy()
+    df[taxon_rank] = df[taxon_rank].astype(str)
 
     tmp_grouped = df.groupby( by=taxon_rank )
-    best_taxa = []
+    best_taxa  = []
+    taxa_added = []
     for column_data in itertools.product( ['reference genome', 'representative genome', 'na'], ['complete genome', 'chromosome', 'scaffold', 'contig'], ['full', 'partial'] ):
         for taxon, taxon_df in tmp_grouped:
-            if column_data in taxon_df['refseq_category assembly_level genome_rep'.split()].values:
+            if list(column_data) in taxon_df['refseq_category assembly_level genome_rep'.split()].values.tolist() and taxon not in taxa_added:
                 best_taxa.append( taxon_df )
+                taxa_added.append( taxon )
 
             if len(best_taxa) == 2:
-                break
-        if len(best_taxa) == 2:
-            break
-
+                return best_taxa
     return best_taxa
 
 def select_representative( tmp_df ):
@@ -85,12 +102,17 @@ genome_lineages  = genome_lineages[ (genome_lineages.phylum.isin( target_taxonom
 merged           = genome_lineages.merge( genbank_summary, how='inner', on='taxid', suffixes=['_lineage','_summary'] )
 
 merged.drop( merged.genus[merged.genus.isnull()].index, axis='index', inplace=True )
+
+pool         = multiprocessing.Pool( processes=20 )
+correct_data = pool.map(test_ftp_path, merged.ftp_path.tolist() )
+pool.close()
+pool.join()
+print 'yeah'
+
+merged = merged[ correct_data ]
+
 grouped_by_genus = merged.groupby( by='genus' )
 selected_genomes = pd.DataFrame( columns=merged.columns )
-single_representative = []
-two_representative    = []
-multiple_species      = []
-multiple_strains      = []
 for genus, df in grouped_by_genus:
     if df.shape[0] == 1:
         selected_genomes = selected_genomes.append( df )
@@ -108,3 +130,5 @@ for genus, df in grouped_by_genus:
                 selected_genomes = selected_genomes.append( select_representative( taxon ) )
             multiple_strains.append( genus )
 print "**\tFiltering done!!!"
+
+selected_genomes.to_csv(   'selected_genomes.tab', sep='\t' )
