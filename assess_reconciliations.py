81cc5b05f7982448e1292168d89d0a0c58d84e2b
thiberio <lthiberiol@gmail.com>
2018-04-10 18:43:43 -0400

    half paralellized...

--- assess_reconciliations.py
+++ assess_reconciliations.py
@@ -9,8 +9,9 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from scipy.stats import pearsonr
 import pandas as pd
+import multiprocessing
 
-class cd:  
+class cd:
     """  
     Context manager for changing the current working directory
     """
@@ -42,25 +43,19 @@ def name_matching_branches( named, unamed ):
 
 os.chdir('/work/Alphas_and_Cyanos')
 
-reference_tree       = ete3.Tree( 'rooted_partitions-with_BB_support.treefile' )
-named_reference_tree = ete3.Tree( 'rooted_partitions-with_named_branches.treefile', format=1 )
-counter              = 0
-supported_transfers  = {}
-for folder in os.listdir( 'reconciliations/' ):
-    if not os.path.isdir( 'reconciliations/%s' %folder ):
-        continue
+def assess_reconciliation(folder):
+    reconciliation_data = {'transfer_supports':[], 'mapping_consistencies':[], 'homologue_group':folder}
 
-    with cd( 'reconciliations/%s' %folder ):
-        reconciliation_files = os.listdir('.')
+    if not os.path.isdir('reconciliations/%s' %folder):
+        return
 
-        if 'aggregate.reconciliation' not in reconciliation_files:
-            continue
-
-        counter += 1
+    with cd('reconciliations/%s' %folder):
+        if not os.path.isfile('aggregate.reconciliation'):
+            return reconciliation_data
 
         #
         # load trees with named branches
-        gene_tree = ete3.Tree( open('%s.reconciliation1' %folder).readlines()[7], format=1 )
+        gene_tree = ete3.Tree(open('%s.reconciliation1' %folder).readlines()[7], format=1 )
         for node in gene_tree.traverse():
             if node.name == 'm1':
                 continue
@@ -70,27 +65,23 @@ for folder in os.listdir( 'reconciliations/' ):
                 continue
 
             node_name, node_support = re.search('^(m\d+?)(100|\d{2})?$', node.name).groups()
-            node.support            = int( node_support if node_support else 0 )
-            node.name               =      node_name
+            node.support            = int(node_support if node_support else 0)
+            node.name               =     node_name
 
         reconciliation = open( 'aggregate.reconciliation' ).read()
 
-        number_of_solutions    = int( re.search( '^Total number of optimal solutions: (\d+)$', reconciliation, re.M ).group(1) )
-        if number_of_solutions > 100000:
-            continue
+        number_of_replications = float(re.match( '^Processed (\d+) files', reconciliation).group(1))
+        events                 = re.findall('^(m\d+\s=.*)$', reconciliation, re.M)
 
-        number_of_replications = float( re.match( '^Processed (\d+) files', reconciliation ).group(1) )
-        events                 = re.findall( '^(m\d+\s=.*)$', reconciliation, re.M )
-
-        flag                = False
+        flag = False
         for event in events:
             transfer_support, mapping_node, mapping_consistency = re.search( ',\sTransfers\s=\s(\d+)], \[Most Frequent mapping --> (\S+), (\d+) times\]', event ).groups()
             transfer_support    = float(transfer_support)
             mapping_consistency = float(mapping_consistency)
-            mapping_node        = named_reference_tree.search_nodes( name=mapping_node)[0] 
 
-            if mapping_node.is_leaf():
-                continue
+            reconciliation_data['transfer_supports'].append(transfer_support/number_of_replications)
+            if transfer_support:
+                reconciliation_data['mapping_consistencies'].append(mapping_consistency/transfer_support)
 
             if transfer_support/number_of_replications >= 1 and transfer_support == mapping_consistency:
                 node_name    = event.split()[0]
@@ -98,6 +89,7 @@ for folder in os.listdir( 'reconciliations/' ):
                     reticulation = gene_tree.search_nodes(name=node_name)[0]
                 except:
                     continue
+
                 if reticulation.support < 95:
                     continue
 
@@ -106,32 +98,48 @@ for folder in os.listdir( 'reconciliations/' ):
 
                 if not flag:
                     flag = True
-                    supported_transfers[folder]              = {}
-                    supported_transfers[folder]['tree']      = gene_tree.copy(method='deepcopy')
-                    supported_transfers[folder]['transfers'] = []
-                supported_transfers[folder]['transfers'].append( event )
+                    reconciliation_data['tree']      = gene_tree.copy(method='deepcopy')
+                    reconciliation_data['transfers'] = []
+                reconciliation_data['transfers'].append( event )
+    return reconciliation_data
+
+pool = multiprocessing.Pool(processes=6)
+results = pool.map(assess_reconciliation, os.listdir('reconciliations/'))
+pool.close()
+pool.join()
 print 'yeah'
 
-rf_values_donor       = []
+reference_tree       = ete3.Tree('rooted_partitions-with_BB_support.treefile')
+named_reference_tree = ete3.Tree('rooted_partitions-with_named_branches.treefile', format=1)
+
+rf_values_donor          = []
 rf_values_norm_donor     = []
-rf_values_recipient   = []
+rf_values_recipient      = []
 rf_values_norm_recipient = []
-final_transfers       = {}
-flag = False
-for gene_family, data in supported_transfers.items():
-    with cd( 'reconciliations/%s' %gene_family ):
+final_transfers          = {}
+counter = 0
+for data in results:
+    counter += 1
+    if counter % 100 == 0:
+        print counter
+
+    if data == None or 'tree' not in data:
+        continue
+
+    flag = False
+    with cd('reconciliations/%s' %data['homologue_group']):
         gene_tree = data['tree']
         for transfer in data['transfers']:
-            grep_query  = re.match( '^(m\d+ = LCA\[\S+, \S+\]:)', transfer, re.M ).group(1)
-            grep_result = getoutput( 'grep "%s" %s.reconciliation*' %(re.escape(grep_query), gene_family ) )
-            grep_result = set(re.sub( '^%s.reconciliation\d+:' %gene_family, '', grep_result, flags=re.M ).split('\n'))
+            grep_query  = re.match('^(m\d+ = LCA\[\S+, \S+\]:)', transfer, re.M).group(1)
+            grep_result = getoutput( 'grep "%s" %s.reconciliation*' %(re.escape(grep_query), data['homologue_group']))
+            grep_result = set(re.sub('^%s.reconciliation\d+:' %data['homologue_group'], '', grep_result, flags=re.M).split('\n'))
             if len(grep_result) > 1:
                 continue
 
             transfer         = grep_result.pop()
-            donor, recipient = re.search( 'Mapping --> (\S+), Recipient --> (\S+)$', transfer, re.M ).groups()
-            recipient_branch = named_reference_tree.search_nodes( name=recipient )[0]
-            donor_branch     = named_reference_tree.search_nodes( name=donor )[0]
+            donor, recipient = re.search('Mapping --> (\S+), Recipient --> (\S+)$', transfer, re.M).groups()
+            recipient_branch = named_reference_tree.search_nodes(name=recipient)[0]
+            donor_branch     = named_reference_tree.search_nodes(name=donor)[0]
             if recipient_branch.is_leaf() or donor_branch.is_leaf():
                 continue
 
@@ -139,17 +147,17 @@ for gene_family, data in supported_transfers.items():
 
             rf, rf_max, names, edges_t1, edges_t2, discarded_edges_t1, discarded_edges_t2 = reticulation.robinson_foulds(donor_branch, attr_t1='genome_name')
             if rf:
-                rf_values_donor.append( rf )
+                rf_values_donor.append(rf)
                 rf_values_norm_donor.append(rf/float(rf_max))
 
             rf, rf_max, names, edges_t1, edges_t2, discarded_edges_t1, discarded_edges_t2 = reticulation.robinson_foulds(recipient_branch, attr_t1='genome_name')
             if rf:
-                rf_values_recipient.append( rf )
+                rf_values_recipient.append(rf)
                 rf_values_norm_recipient.append(rf/float(rf_max))
 
-            if gene_family not in final_transfers:
-                final_transfers[gene_family] = []
-            final_transfers[gene_family].append(transfer)
+            if not flag:
+                final_transfers[data['homologue_group']] = []
+            final_transfers[data['homologue_group']].append(transfer)
 print 'yeah'
 
 fig, axs = plt.subplots(nrows=2)
@@ -158,7 +166,7 @@ sns.distplot(rf_values_donor,     ax=axs[0])
 axs[1].set_title('Recipient branch Robinson-Foulds distances')
 sns.distplot(rf_values_recipient, ax=axs[1])
 fig.tight_layout()
-fig.savefig('rf_distances.pdf', dpi=300)
+fig.savefig('rf_distances.pdf', dpi=600)
 
 fig, axs = plt.subplots(nrows=2)
 axs[0].set_title('Donor branch normalized Robinson-Foulds distances')
@@ -166,7 +174,15 @@ sns.distplot(rf_values_norm_donor,     ax=axs[0])
 axs[1].set_title('Recipient branch normalized Robinson-Foulds distances')
 sns.distplot(rf_values_norm_recipient, ax=axs[1])
 fig.tight_layout()
-fig.savefig('rf_distances_norm.pdf', dpi=300)
+fig.savefig('rf_distances_norm.pdf', dpi=600)
+
+fig, axs = plt.subplots(nrows=2)
+axs[0].set_title('Ranger HGT confidences')
+sns.distplot(transfer_supports,     ax=axs[0])
+axs[1].set_title('Ranger mapping consistencies')
+sns.distplot(mapping_consistencies, ax=axs[1])
+fig.tight_layout()
+fig.savefig('ranger_support_metrics.pdf', dpi=600)
 
 
 # test distance to root and branch support correlation #####
