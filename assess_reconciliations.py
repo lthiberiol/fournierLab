9871c3a07be237a02c43b8ce6ae1494f2cc0aea1
thiberio <lthiberiol@gmail.com>
2018-04-13 19:11:32 -0400

    ...

--- assess_reconciliations.py
+++ assess_reconciliations.py
@@ -5,9 +5,11 @@ import ete3
 import os
 import re
 from commands import getoutput
+import multiprocessing
+import itertools
+import pickle as pkl
 import seaborn as sns
 from matplotlib import pyplot as plt
-import multiprocessing
 
 class cd:
     """  
@@ -23,28 +25,39 @@ class cd:
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
 
-def name_matching_branches( named, unamed ):
-    if named.get_topology_id() == unamed.get_topology_id():
-        unamed.name = named.name
-
-    for node1 in unamed.children:
-        if node1.is_leaf():
-            continue
-
-        for node2 in named.children:
-            if node2.is_leaf():
-                continue
-
-            if node1.get_topology_id() == node2.get_topology_id():
-                node1.name = node2.name
-                name_matching_branches( node2, node1 )
-
 os.chdir('/work/Alphas_and_Cyanos')
 named_reference_tree = ete3.Tree('rooted_partitions-with_named_branches.treefile', format=1)
 
+def root_like( ref_tree, tree_to_root ):
+    outgroup = ''
+    for node in sorted( ref_tree.children, key=len ):
+        if node.is_leaf():
+            putative_outgroups = tree_to_root.get_leaves_by_name(node.name)
+            for leaf in putative_outgroups:
+                tree_to_root.set_outgroup( leaf )
+                if tree_to_root.get_topology_id() == ref_tree.get_topology_id():
+                    outgroup = leaf
+                    break
+        else:
+            outgroup_members = []
+            for leaf in node:
+                outgroup_members.append( tree_to_root.get_leaves_by_name(leaf.name) )
+
+            for outgroup_combination in itertools.product( *outgroup_members ):
+                if tree_to_root.check_monophyly( [x.name for x in outgroup_combination], 'name' )[0]:
+                    putative_outgroup = tree_to_root.get_common_ancestor( outgroup_combination )
+                    tree_to_root.set_outgroup( putative_outgroup )
+                    if tree_to_root.get_topology_id() == ref_tree.get_topology_id():
+                        outgroup = putative_outgroup
+                        break
+        if outgroup:
+            return tree_to_root
+
+    if not outgroup:
+        return None
+
 def assess_branch_compatibility(folder, transfers, gene_tree, named_reference_tree):
-    final_transfers = []
-    rf_values = {'donor':[], 'recipient':[], 'donor_normed':[], 'recipient_normed':[]}
+    selected_transfers = []
     for transfer in transfers:
         grep_query  = re.match('^(m\d+ = LCA\[\S+, \S+\]:)', transfer, re.M).group(1)
         grep_result = getoutput( 'grep "%s" %s.reconciliation*' %(re.escape(grep_query), folder))
@@ -68,14 +81,14 @@ def assess_branch_compatibility(folder, transfers, gene_tree, named_reference_tr
         #
         # assess donor branch compatibility
         rf, rf_max, names, edges_t1, edges_t2, discarded_edges_t1, discarded_edges_t2 = reticulation.robinson_foulds(donor_branch, attr_t1='genome_name')
-        if rf:
-            return None
+        if rf_max and rf/rf_max > 0.2:
+            continue
 
         #
         # assess recipient branch compatibility
         rf, rf_max, names, edges_t1, edges_t2, discarded_edges_t1, discarded_edges_t2 = reticulation.robinson_foulds(recipient_branch, attr_t1='genome_name')
-        if rf:
-            return None
+        if rf_max and rf/rf_max > 0.2:
+            continue
 
         donor_intersection     = [leaf.name for leaf in reticulation.get_leaves() if leaf.genome_name in donor_branch.get_leaf_names()    ]
         recipient_intersection = [leaf.name for leaf in reticulation.get_leaves() if leaf.genome_name in recipient_branch.get_leaf_names()]
@@ -86,23 +99,11 @@ def assess_branch_compatibility(folder, transfers, gene_tree, named_reference_tr
         donor_branch_in_reticulation     = reticulation.get_common_ancestor(donor_intersection    )
         recipient_branch_in_reticulation = reticulation.get_common_ancestor(recipient_intersection)
 
-        useful_topology = False
         if donor_branch_in_reticulation in recipient_branch_in_reticulation.get_ancestors():
             print '%s: recipient nested into donor' %folder
-            useful_topology = True
-        elif recipient_branch_in_reticulation in donor_branch_in_reticulation.get_ancestors():
-            print '%s: donor nested into recipient' %folder
-        elif donor_branch_in_reticulation in recipient_branch_in_reticulation.get_sisters():
-            print '%s: donor and recipient are sisters' %folder
-            useful_topology = True
+            selected_transfers.append(transfer)
 
-        if not useful_topology:
-            continue
-
-        final_transfers.append(transfer)
-    gene_tree.ladderize()
-    gene_tree.write(outfile='/work/Alphas_and_Cyanos/%s.tre' %folder, format=1)
-    return {folder:final_transfers}
+    return {folder:selected_transfers}
 
 def assess_reconciliation(folder):
     reconciliation_data = {'transfer_supports':[], 'mapping_consistencies':[], 'homologue_group':folder}
@@ -113,20 +114,23 @@ def assess_reconciliation(folder):
     with cd('reconciliations/%s' %folder):
         #
         # load trees with named branches
-        gene_tree = ete3.Tree(open('%s.reconciliation1' %folder).readlines()[7], format=1 )
-        for node in gene_tree.traverse():
-            if node.name == 'm1':
-                continue
+        named_gene_tree = ete3.Tree(open('%s.reconciliation1' %folder).readlines()[7], format=1 )
 
-            if node.is_leaf():
-                node.add_feature('genome_name', node.name.split('_')[0])
-                continue
+        gene_tree       = ete3.Tree('../../iqtrees/%s.aln.treefile'      %folder, format=0)
+        for leaf in gene_tree.get_leaves():
+            leaf.name = re.sub('\.\d+', '', leaf.name.replace('GCA_', 'GCA'))
+            leaf.add_feature('genome_name', leaf.name.split('_')[0])
 
-            node_name, node_support = re.search('^(m\d+?)(100|\d{2})?$', node.name).groups()
-            node.support            = int(node_support if node_support else 0)
-            node.name               =     node_name
+        if set(named_gene_tree.get_leaf_names()).issubset(gene_tree.get_leaf_names()):
+            gene_tree.prune(named_gene_tree.get_leaf_names())
+        else:
+            print 'FUUUUUCK'
+        gene_tree = root_like(named_gene_tree, gene_tree)
 
         reconciliation = open( 'aggregate.reconciliation' ).read()
+        for node_name, descendant1, descendant2 in re.findall('^(m\d+)\s=\sLCA\[(\S+),\s(\S+)\]', reconciliation, re.M):
+            branch      = gene_tree.get_common_ancestor([descendant1, descendant2])
+            branch.name = node_name
 
         number_of_replications = float(re.match( '^Processed (\d+) files', reconciliation).group(1))
         events                 = re.findall('^(m\d+\s=.*)$', reconciliation, re.M)
@@ -164,9 +168,11 @@ def assess_reconciliation(folder):
             return None
         return assess_branch_compatibility(folder, reconciliation_data['transfers'], gene_tree, named_reference_tree)
 
+results = map(assess_reconciliation, os.listdir('reconciliations/'))
+
 #with multiprocessing.Pool(processes=6) as pool:
 pool = multiprocessing.Pool(processes=10)
-results1 = pool.map(assess_reconciliation, os.listdir('reconciliations/'))
+results = pool.map(assess_reconciliation, os.listdir('reconciliations/'))
 pool.close()
 pool.join()
 
@@ -175,6 +181,10 @@ for element in results:
     if type(element) is not dict or element.values() == [[]]:
         continue
     final_transfers.update(element)
+
+out = open('final_transfers.pkl', 'wb')
+pkl.dump(final_transfers, out)
+out.close()
 print 'yeah'
 
 ### fig, axs = plt.subplots(nrows=2)
@@ -193,39 +203,39 @@ print 'yeah'
 ### fig.tight_layout()
 ### fig.savefig('rf_distances_norm.pdf', dpi=600)
 ###
-transfer_supports     = []
-mapping_consistencies = []
-for element in results1:
-    if element == None:
-        continue
-    transfer_supports.extend(element[0])
-    mapping_consistencies.extend(element[1])
-fig, axs = plt.subplots(nrows=2)
-axs[0].set_title('Ranger HGT confidences')
-sns.distplot(transfer_supports,     ax=axs[0])
-axs[1].set_title('Ranger mapping consistencies')
-sns.distplot(mapping_consistencies, ax=axs[1])
-fig.tight_layout()
-fig.savefig('ranger_support_metrics.pdf', dpi=600)
-
-farthest_leaf_from_root   = named_reference_tree.get_farthest_leaf(   topology_only=True)[0]
-longest_possible_distance = farthest_leaf_from_root.get_farthest_node(topology_only=True)[1]
-bipartition_distances        = []
-bipartition_distances_normed = []
-donor_distance_to_root       = []
-for transfers in final_transfers.values():
-    for transfer in transfers:
-        donor, recipient = re.search('Mapping --> (\S+), Recipient --> (\S+)$', transfer, re.M).groups()
-        recipient_branch = named_reference_tree.search_nodes(name=recipient)[0]
-        donor_branch     = named_reference_tree.search_nodes(name=donor    )[0]
-        bipartition_distances.append(donor_branch.get_distance(recipient_branch, topology_only=True))
-        bipartition_distances_normed.append(donor_branch.get_distance(recipient_branch, topology_only=True)/longest_possible_distance)
-        donor_distance_to_root.append(named_reference_tree.get_distance(donor_branch, topology_only=True))
-
-fig, axs = plt.subplots(nrows=2)
-axs[0].set_title('# of bipartitions from root to donor')
-sns.distplot(donor_distance_to_root, ax=axs[0])
-axs[1].set_title('# of bipartitions between donor/recipient (normalized)')
-sns.distplot(bipartition_distances_normed, ax=axs[1])
-fig.tight_layout()
-fig.savefig('hgt_distances.pdf', dpi=600)
+### transfer_supports     = []
+### mapping_consistencies = []
+### for element in results1:
+###     if element == None:
+###         continue
+###     transfer_supports.extend(element[0])
+###     mapping_consistencies.extend(element[1])
+### fig, axs = plt.subplots(nrows=2)
+### axs[0].set_title('Ranger HGT confidences')
+### sns.distplot(transfer_supports,     ax=axs[0])
+### axs[1].set_title('Ranger mapping consistencies')
+### sns.distplot(mapping_consistencies, ax=axs[1])
+### fig.tight_layout()
+### fig.savefig('ranger_support_metrics.pdf', dpi=600)
+###
+### farthest_leaf_from_root   = named_reference_tree.get_farthest_leaf(   topology_only=True)[0]
+### longest_possible_distance = farthest_leaf_from_root.get_farthest_node(topology_only=True)[1]
+### bipartition_distances        = []
+### bipartition_distances_normed = []
+### donor_distance_to_root       = []
+### for transfers in final_transfers.values():
+###     for transfer in transfers:
+###         donor, recipient = re.search('Mapping --> (\S+), Recipient --> (\S+)$', transfer, re.M).groups()
+###         recipient_branch = named_reference_tree.search_nodes(name=recipient)[0]
+###         donor_branch     = named_reference_tree.search_nodes(name=donor    )[0]
+###         bipartition_distances.append(donor_branch.get_distance(recipient_branch, topology_only=True))
+###         bipartition_distances_normed.append(donor_branch.get_distance(recipient_branch, topology_only=True)/longest_possible_distance)
+###         donor_distance_to_root.append(named_reference_tree.get_distance(donor_branch, topology_only=True))
+###
+### fig, axs = plt.subplots(nrows=2)
+### axs[0].set_title('# of bipartitions from root to donor')
+### sns.distplot(donor_distance_to_root, ax=axs[0])
+### axs[1].set_title('# of bipartitions between donor/recipient (normalized)')
+### sns.distplot(bipartition_distances_normed, ax=axs[1])
+### fig.tight_layout()
+### fig.savefig('hgt_distances.pdf', dpi=600)
--- index_hgt_visualization.py
+++ index_hgt_visualization.py
@@ -5,10 +5,8 @@ import ete3
 import os
 import itertools
 import re
-from commands import getoutput
-import seaborn as sns
-from matplotlib import pyplot as plt
-import multiprocessing
+import pandas as pd
+from Bio import Phylo
 
 class cd:
     """
@@ -24,28 +22,7 @@ class cd:
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
 
-def name_matching_branches( named, unamed ):
-    if named.get_topology_id() == unamed.get_topology_id():
-        unamed.name = named.name
-
-    for node1 in unamed.children:
-        if node1.is_leaf():
-            continue
-
-        for node2 in named.children:
-            if node2.is_leaf():
-                continue
-
-            if node1.get_topology_id() == node2.get_topology_id():
-                node1.name = node2.name
-                name_matching_branches( node2, node1 )
-
 def root_like( ref_tree, tree_to_root ):
-    if len(ref_tree) < len(tree_to_root):
-        tree_to_root.prune(ref_tree.get_leaf_names())
-    elif len(ref_tree) > len(tree_to_root):
-        print 'Different tree sizes'
-        return
     outgroup = ''
     for node in sorted( ref_tree.children, key=len ):
         if node.is_leaf():
@@ -71,35 +48,93 @@ def root_like( ref_tree, tree_to_root ):
             return tree_to_root
 
     if not outgroup:
-        print 'fuck'
         return None
 
 os.chdir('/work/Alphas_and_Cyanos/groups_with_nested_transfers')
+named_reference_tree = ete3.Tree('../rooted_partitions-with_named_branches.treefile', format=1)
+final_transfers = {'003575': ['m83 = LCA[GCA000012905_ABA79637, GCA001459775_CUU43052]: Transfer, Mapping --> n262, Recipient --> n297'],
+ '004119': ['m16 = LCA[GCA000019945_ACB79599, GCA900100665_SDG33339]: Transfer, Mapping --> n134, Recipient --> n293']}
+
+genome_table       = pd.read_table('../selected_genomes.tab', index_col=31)
+genome_table.index = [index.replace('_', '').split('.')[0] for index in genome_table.index]
+
+reticulation_style = ete3.NodeStyle()
+donor_style        = ete3.NodeStyle()
+recipient_style    = ete3.NodeStyle()
+
+#reticulation_style["fgcolor"]       = "#0f0f0f"
+#reticulation_style["size"]          = 0
+reticulation_style["vt_line_color"] = "#ff0000"
+reticulation_style["hz_line_color"] = "#ff0000"
+reticulation_style["vt_line_width"] = 5
+reticulation_style["hz_line_width"] = 5
+reticulation_style["vt_line_type"]  = 1
+reticulation_style["hz_line_type"]  = 0
+
+donor_style[    'bgcolor']          = 'LightSteelBlue'
+recipient_style['bgcolor']          = 'DarkSeaGreen'
 
 homologue_group = '004119'
-named_gene_tree = ete3.Tree('%s.optimal_root.tree'        %homologue_group, format=1)
-for node in named_gene_tree.traverse():
-    if node.name == 'm1' or node.is_leaf() or not node.name:
-        continue
+named_gene_tree = ete3.Tree('%s.optimal_root.tree' %homologue_group, format=1)
+gene_tree       = ete3.Tree('../alignments/%s.aln.treefile'      %homologue_group, format=0)
+for leaf in gene_tree.get_leaves():
+    leaf.name = re.sub('\.\d+', '', leaf.name.replace('GCA_', 'GCA'))
 
-    node_name, node_support = re.search('^(m\d+?)(100|\d{2})?$', node.name).groups()
-    node.support            = int(node_support if node_support else 0)
-    node.name               =     node_name
+if set(named_gene_tree.get_leaf_names()).issubset(gene_tree.get_leaf_names()):
+    gene_tree.prune(named_gene_tree.get_leaf_names())
+gene_tree = root_like(named_gene_tree, gene_tree)
 
-gene_tree       = ete3.Tree('RAxML_result.%s' %homologue_group)
+for node_name, descendant1, descendant2 in re.findall('^(m\d+)\s=\sLCA\[(\S+),\s(\S+)\]', open('../reconciliations/%s/aggregate.reconciliation' %homologue_group).read(), re.M):
+    branch      = gene_tree.get_common_ancestor([descendant1, descendant2])
+    branch.name = node_name
 
-for named_node in named_gene_tree.traverse():
-    if named_node.is_leaf():
-        continue
+for transfer in final_transfers[homologue_group]:
+    reticulation_name = transfer.split()[0]
+    reticulation      = gene_tree.search_nodes(name=reticulation_name)[0]
+    for node in reticulation.traverse():
+        if node.is_leaf():
+            node.add_feature('genome_name', node.name.split('_')[0])
 
-    equivalent_node = gene_tree.get_common_ancestor(named_node.get_leaf_names())
-    if len(named_node) != len(equivalent_node):
-        print named_node.name
-        break
+    donor_name, recipient_name = re.search('Mapping --> (\S+), Recipient --> (\S+)$', transfer, re.M).groups()
+    donor_branch               = named_reference_tree.search_nodes(name=donor_name    )[0]
+    recipient_branch           = named_reference_tree.search_nodes(name=recipient_name)[0]
+
+    donor_intersection     = [leaf.name for leaf in reticulation.get_leaves() if leaf.genome_name in donor_branch.get_leaf_names()    ]
+    recipient_intersection = [leaf.name for leaf in reticulation.get_leaves() if leaf.genome_name in recipient_branch.get_leaf_names()]
+
+    donor_branch_in_reticulation     = reticulation.get_common_ancestor(donor_intersection    )
+    recipient_branch_in_reticulation = reticulation.get_common_ancestor(recipient_intersection)
 
 
+    reticulation.set_style(reticulation_style)
+    donor_branch_in_reticulation.set_style(        donor_style)
+    recipient_branch_in_reticulation.set_style(recipient_style)
 
+##color palette: blue, red, green , orange, purple, black
+#color_palette   = '#2e2be2 #e22e2b #2be22e #f9a82c #8a2be2 #000000'
+nameFaces       = {}
+nameFaces[1117] = ete3.AttrFace("name", fsize=10, fgcolor='#000000')
+nameFaces[1224] = ete3.AttrFace("name", fsize=10, fgcolor='#a20417')
+def myLayout(node):
+    if node.is_leaf():
+        node.img_style['size'] = 0
+        genome_name, gene_name = node.name.split('_')
+        node.name = genome_table.loc[genome_name, 'organism_name']
+        ete3.add_face_to_node(nameFaces[genome_table.loc[genome_name, 'phylum']], node, 2, aligned=True)
+    elif node.is_root():
+        node.img_style['size'] = 0
+    else:
+        if node.support >= 90:
+            node.img_style['size'] = 5
+            node.img_style['fgcolor'] = '#a20417'
+        else:
+            node.img_style['size'] = 0
 
-name_matching_branches(named_gene_tree, gene_tree)
-named_gene_tree.get_topology_id()
-gene_tree.get_topology_id()
+treeStyle                    = ete3.TreeStyle()
+treeStyle.layout_fn          = myLayout
+treeStyle.show_leaf_name     = False
+treeStyle.draw_guiding_lines = True
+tree_to_plot                 = gene_tree.copy(method='deepcopy')
+
+tree_to_plot.write(outfile='%s.tree' %homologue_group, format=1)
+tree_plot = tree_to_plot.render('%s.pdf' %homologue_group, tree_style=treeStyle, dpi=600)
