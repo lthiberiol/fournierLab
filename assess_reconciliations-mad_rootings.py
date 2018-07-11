ccaf23668468736376556b799a90b8cca1df2890
thiberio <lthiberiol@gmail.com>
2018-07-10 20:55:33 -0400

    new version dealing with simpler aggregate process

--- assess_reconciliations-mad_rootings.py
+++ assess_reconciliations-mad_rootings.py
@@ -7,8 +7,17 @@ import re
 import multiprocessing
 import pickle as pkl
 import linecache
+import pandas as pd
 
 os.chdir('/work/Alphas_and_Cyanos')
+ncbi = ete3.NCBITaxa()
+header = 'assembly_accession bioproject biosample wgs_master refseq_category taxid species_taxid organism_name infraspecific_name isolate version_status assembly_level release_type genome_rep seq_rel_date asm_name submitter gbrs_paired_asm paired_asm_comp ftp_path excluded_from_refseq relation_to_type_material'.split()
+genbank_summary                     = pd.read_table('/work/assembly_summary_genbank.txt', comment='#', header=None, names=header, dtype={'taxid':str, 'infraspecific_name':str})
+genbank_summary['refseq_category']  = genbank_summary['refseq_category'].str.lower()
+genbank_summary['assembly_level']   = genbank_summary['assembly_level'].str.lower()
+genbank_summary['genome_rep']       = genbank_summary['genome_rep'].str.lower()
+genbank_summary.set_index('assembly_accession', inplace=True)
+genbank_summary.index               = [re.sub('\.\d+$', '', index).replace('_', '') for index in genbank_summary.index]
 
 class cd:
     """
@@ -86,9 +95,10 @@ def parse_aggregated(folder, threshold=0.9, leaves_allowed=False):
             recipient_map_name = recipient_map_search.group(1)
             selected_transfers.append({'donor':donor_name, 'recipient':recipient_name, 'donor_map':donor_map_name, 'recipient_map':recipient_map_name})
 
-    return {folder:selected_transfers}
+    return {folder:[selected_transfers, gene_tree]}
 
 with cd('reconciliations/mad_roots'):
+    yeah = map(parse_aggregated, '000049 000050'.split())
     pool = multiprocessing.Pool(processes=4)
     transfers = pool.map(parse_aggregated, os.listdir('.'))
     pool.close()
@@ -101,4 +111,132 @@ with cd('reconciliations/mad_roots'):
 
 out = open('aggregated/mad-90_threshold.pkl', 'wb')
 pkl.dump(yeah, out)
+out.close()
+
+reference_tree = ete3.Tree('rooted_partitions-with_named_branches.treefile', format=1)
+def visualize_tree(group, transfers, tree, output_folder='index_transfer_trees/mad-90_threshold'):
+    gene_tree = tree.copy()
+    tmp_names = {}
+
+    for leaf in gene_tree.get_leaves():
+        genome_name, gene_name = leaf.name.split('_')
+        leaf.add_feature('genome', genome_name)
+
+    count = 0
+    for mappings in transfers:
+        donor_map     = gene_tree.search_nodes(name=mappings['donor_map']    )[0]
+        recipient_map = gene_tree.search_nodes(name=mappings['recipient_map'])[0]
+
+        donor_branch = reference_tree.search_nodes(name=mappings[ 'donor'])[0]
+        if 'annotation' not in donor_branch.features:
+            donor_branch.add_feature('annotation', {'donor':[], 'recipient':[]})
+        recipient_branch = reference_tree.search_nodes(name=mappings['recipient'])[0]
+        if 'annotation' not in recipient_branch.features:
+            recipient_branch.add_feature('annotation', {'donor':[], 'recipient':[]})
+
+        donor_branch.annotation['donor'].append([group, count])
+        recipient_branch.annotation['recipient'].append([group, count])
+
+        #
+        # set donor name
+        if not donor_map.name in tmp_names:
+            tmp_names[donor_map.name] = {'roles':[], 'hgt_count':[]}
+        tmp_names[donor_map.name]['roles'].append('donor%i' %count)
+        tmp_names[donor_map.name]['hgt_count'].append(str(count))
+
+        #
+        # set recipient name
+        if not recipient_map.name in tmp_names:
+            tmp_names[recipient_map.name] = {'roles':[], 'hgt_count':[]}
+        tmp_names[recipient_map.name]['roles'].append('recipient%i' %count)
+        tmp_names[recipient_map.name]['hgt_count'].append(str(count))
+
+        count += 1
+        del(donor_map, recipient_map)
+
+    features = {}
+    for node_name, feats in tmp_names.items():
+        node   = gene_tree.search_nodes(name=node_name)[0]
+        tmp_id = str(random.random())
+        features[tmp_id] = '[&support=%.2f,hgt_role="%s",hgt_count="%s"]' %(node.support, ', '.join(feats['roles']), ', '.join(feats['hgt_count']))
+        node.name = tmp_id
+
+
+    out  = open('%s/%s.Figtree.tree' %(output_folder, group), 'wb')
+    out.write("#NEXUS\nbegin taxa;\n\tdimensions ntax=%i;\n\ttaxlabels\n" %len(gene_tree))
+    for node in gene_tree.traverse():
+        if node.is_leaf():
+            taxid         = genbank_summary.loc[node.genome, 'taxid']
+            lineage       = {j: i for i, j in ncbi.get_rank(ncbi.get_lineage(taxid)).items()}
+            lineage_names = ncbi.get_taxid_translator(lineage.values())
+
+            out.write('\t%s ' %(node.name))
+            comment = ['source_name="%s"' %ncbi.get_taxid_translator([taxid]).itervalues().next()]
+            for rank in ['phylum', 'class', 'order', 'family', 'genus']:
+                if rank in lineage:
+                    comment.append('tax_%s="%s"' %(rank, lineage_names[lineage[rank]]))
+            out.write('[&%s]\n' %' '.join(comment))
+
+        else:
+            if node.support and not node.name:
+                node.name = '[&support=%.2f]' %node.support
+
+    newick_text = gene_tree.write(format=1)
+    newick_text = re.sub('_&support_(\d+\.\d\d)_', '[&support=\\1]', newick_text)
+    for key, value in features.items():
+        newick_text = newick_text.replace(key, value)
+    out.write(';\nend;\n')
+    out.write('begin trees;\n\ttree tree_1 = [&R] %s\nend;' %newick_text)
+    out.close()
+
+#secure_copy = reference_tree.copy('deepcopy')
+
+out  = open('index_transfer_trees/mad-90_threshold/species_tree.Figtree.tree', 'wb')
+out.write("#NEXUS\nbegin taxa;\n\tdimensions ntax=%i;\n\ttaxlabels\n" %len(reference_tree))
+tmp_names = {}
+for node in reference_tree.traverse():
+    if node.is_leaf():
+        taxid = genbank_summary.loc[node.name, 'taxid']
+        lineage = {j: i for i, j in ncbi.get_rank(ncbi.get_lineage(taxid)).items()}
+        lineage_names = ncbi.get_taxid_translator(lineage.values())
+
+        out.write('\t%s ' % (node.name))
+        comment = ['source_name="%s"' % ncbi.get_taxid_translator([taxid]).itervalues().next()]
+        for rank in ['class', 'phylum', 'order']:
+            if rank in lineage:
+                comment.append('tax_%s="%s"' % (rank, lineage_names[lineage[rank]]))
+        out.write('[&%s]\n' %' '.join(comment))
+    else:
+        if 'annotation' in node.features:
+            tmp_id            = str(random.random())
+            tmp_names[tmp_id] = {'ranger_name':node.name}
+            node.name         = tmp_id
+            for group, count in node.annotation['donor']:
+                if group not in tmp_names[tmp_id]:
+                    tmp_names[tmp_id][group] = {'donor':[], 'recipient':[]}
+                tmp_names[tmp_id][group]['donor'].append(str(count))
+            for group, count in node.annotation['recipient']:
+                if group not in tmp_names[tmp_id]:
+                    tmp_names[tmp_id][group] = {'donor':[], 'recipient':[]}
+                tmp_names[tmp_id][group]['recipient'].append(str(count))
+
+newick_text = reference_tree.write(format=1)
+for key, value in tmp_names.items():
+    comment = []
+    for group, roles in value.items():
+        if group == 'ranger_name':
+            continue
+        donor_fragment = ''
+        if roles['donor']:
+            donor_fragment = 'donor%s' %','.join(roles['donor'])
+
+        recipient_fragment = ''
+        if roles['recipient']:
+            recipient_fragment = 'recipient%s' %','.join(roles['recipient'])
+
+        comment.append('%s_role="%s %s"' %(group, donor_fragment, recipient_fragment) )
+
+    newick_text = newick_text.replace(key, '[&ranger_name="%s",%s]' %(value['ranger_name'], ','.join(comment)))
+out.write(';\nend;\n')
+out.write('begin trees;\n\ttree tree_1 = [&R] %s\nend;' %newick_text)
 out.close()
